aa2b213b89e48a3b4a78009c6257156163e66eb3
GiovanniS <giovanni.squillace.work@gmail.com>
2024-11-02 18:30:50 -0400

    Create two functions, one to create an sql database from counties_with_states.csv and the other to ensure thatdatabase exists

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,7 +1,12 @@
 import censusdis.data as ced
 from censusdis.datasets import ACS5
 import censusdis.states as states
+import pandas as pd
 import os
+import sqlite3
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
 
 def download_list_of_counties():
 	counties_df = ced.download(
@@ -12,6 +17,14 @@ def download_list_of_counties():
 	    county="*"
 	)
 	counties_df[['county', 'state']] = counties_df['NAME'].str.split(', ', expand=True)
-	counties_df.drop(columns=['NAME', 'STATE', 'COUNTY'], inplace=True)
-	current_directory = os.path.dirname(os.path.abspath(__file__))
+	counties_df.drop(columns=['NAME', 'STATE', 'COUNTY'], inplace=True)  # We don't need these columns
 	counties_df.to_csv(f'{current_directory}/../data/processed/counties_with_states.csv', index=False)
+
+
+def create_sql_database():
+	df = pd.read_csv(f'{current_directory}/../data/processed/counties_with_states.csv')
+	with sqlite3.connect(f'{current_directory}/../data/sql_data/CountiesByState.db') as connect:
+		for state in df['state'].unique():
+			state_counties = df.loc[ df['state'] == state ][['county']]
+			# Create the tables and populate them
+			state_counties.to_sql(state, connect, if_exists='replace', index=False)
--- src/helpers.py
+++ src/helpers.py
@@ -1,11 +1,24 @@
 import os
 from src import data_collection
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+
 def ensure_county_data_is_installed():
-	current_directory = os.path.dirname(os.path.abspath(__file__))
 	file_path = f"{current_directory}/../data/processed/counties_with_states.csv"
 	if not os.path.exists(file_path):
-		print("data not installed")
+		print("counties_with_states.csv not found. Downloading data now...")
 		data_collection.download_list_of_counties()
+		print("\033[32m  \u2713 Download complete. \033[0m")
 	else:
-		print("nothing to do")
+		print("\033[32m  \u2713 County CSV found. \033[0m")
+
+
+def ensure_sql_database_exists():
+	file_path = f"{current_directory}/../data/sql_data/CountiesByState.db"
+	if not os.path.exists(file_path):
+		print('CountiesByState.db not found. Creating database...')
+		data_collection.create_sql_database()
+		print('\033[32m  \u2713 SQL Database created. \033[0m')
+	else:
+		print("\033[32m  \u2713 SQL Database found. \033[0m")
