2c6ce9852273a9592085b208190d62f960e957e7
GiovanniS <giovanni.squillace.work@gmail.com>
2024-11-02 18:49:43 -0400

    Add color and styling for the terminal for better UI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from src import helpers
 
-print("----- Prerequisite Checks -----")
+print("\n----- Prerequisite Checks -----\n")
 helpers.ensure_county_data_is_installed()
 helpers.ensure_sql_database_exists()
+print("\n----- Prerequisite Complete! -----\n")
--- src/helpers.py
+++ src/helpers.py
@@ -7,7 +7,7 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 def ensure_county_data_is_installed():
 	file_path = f"{current_directory}/../data/processed/counties_with_states.csv"
 	if not os.path.exists(file_path):
-		print("counties_with_states.csv not found. Downloading data now...")
+		print("\033[33m CSV data not found. Downloading data now... \033[0m")
 		data_collection.download_list_of_counties()
 		print("\033[32m  \u2713 Download complete. \033[0m")
 	else:
@@ -17,8 +17,8 @@ def ensure_county_data_is_installed():
 def ensure_sql_database_exists():
 	file_path = f"{current_directory}/../data/sql_data/CountiesByState.db"
 	if not os.path.exists(file_path):
-		print('CountiesByState.db not found. Creating database...')
+		print('\033[33m SQL database not found. Creating database... \033[0m')
 		data_collection.create_sql_database()
-		print('\033[32m  \u2713 SQL Database created. \033[0m')
+		print('\033[32m  \u2713 Database created. \033[0m')
 	else:
 		print("\033[32m  \u2713 SQL Database found. \033[0m")
