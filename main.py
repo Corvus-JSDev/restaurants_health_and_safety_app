0577d3e0da6b57d1500947fae1acdc02cf5fc449
GiovanniS <giovanni.squillace.work@gmail.com>
2024-11-03 17:13:11 -0500

    Set up streamlit webapp along with themeing

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import streamlit as st
+import streamlit as st  # python3 -m streamlit run main.py
 from src import helpers
 
 # Print the among us character
 amongus = chr(sum(range(ord(min(str(not()))))))
 
-print(f"\n----- Prerequisite Checks -----\n")
-helpers.ensure_county_data_is_installed()
-helpers.ensure_sql_database_exists()
-print(f"\n----- Prerequisite Complete! -----\n")
+# print(f"\n----- Prerequisite Checks -----\n")
+# helpers.ensure_county_data_is_installed()
+# helpers.ensure_sql_database_exists()
+# print(f"\n----- Prerequisite Complete! -----\n")
+
+st.write("""
+	# Header hello
+	This is a first attempted at getting streamlit working :)
+	""")
+st.write("Hello world")
